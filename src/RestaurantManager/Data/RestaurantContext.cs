75408f2c65d8da08ee64142218d1d9452f20f293
Joseph Izang <joseph@pop-os.localdomain>
2020-07-05 02:34:49 +0100

    first commit

---
+++ src/RestaurantManager/Data/RestaurantContext.cs
@@ -0,0 +1,21 @@
+﻿using RestaurantManager.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantManager.Data
+{
+    public class RestaurantContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public RestaurantContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
